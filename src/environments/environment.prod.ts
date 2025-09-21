37529a9ecedbc9df7c0cf280551b0ee65527ae13
VIKASHNAGAR786 <vikashnagar872@gmail.com>
2025-09-21 20:47:33 +0530

    make changes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
 production: true,
-AccountApiUrl : 'http://localhost:5139/Account/',
-BASE_URL: 'http://localhost:5139',
+AccountApiUrl : 'http://localhost:5000/Account/',
+BASE_URL: 'http://localhost:5000',
 };
