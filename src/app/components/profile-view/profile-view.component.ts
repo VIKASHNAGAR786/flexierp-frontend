1263290a49c1b23a7b5f04f4f68d6bbbbc910113
VIKASHNAGAR786 <vikashnagar872@gmail.com>
2025-09-29 12:19:53 +0530

    Enhance ProfileView component with editable profile functionality and modal for editing details

--- src/app/components/profile-view/profile-view.component.ts
+++ src/app/components/profile-view/profile-view.component.ts
@@ -1,13 +1,31 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-profile-view',
   templateUrl: './profile-view.component.html',
-  styleUrls: ['./profile-view.component.css']
+  styleUrls: ['./profile-view.component.css'],
+  imports: [CommonModule, FormsModule],
 })
 export class ProfileViewComponent {
   // Dummy data - later connect with backend
   username = 'John Doe';
   email = 'john.doe@example.com';
   phone = '+91 98765 43210';
+
+  showwditprofile = false;
+
+  editableProfile : any = {};
+
+  editingProfile() {
+    this.showwditprofile = !this.showwditprofile;
+  }
+  closeEdit() {
+    this.showwditprofile = false;
+  }
+  saveProfile() {
+    // Save logic here
+    this.showwditprofile = false;
+  }
 }
