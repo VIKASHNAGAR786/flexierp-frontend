943e9d8859eadfe10057a4e36b8d3027bb30f0e4
VIKASHNAGAR786 <vikashnagar872@gmail.com>
2025-10-08 11:03:08 +0530

    port 5139

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
 production: false,
-AccountApiUrl : 'http://localhost:5000/Account/',
-BASE_URL: 'http://localhost:5000',
+AccountApiUrl : 'http://localhost:5139/Account/',
+BASE_URL: 'http://localhost:5139',
 };
 
 // export const environment = {
