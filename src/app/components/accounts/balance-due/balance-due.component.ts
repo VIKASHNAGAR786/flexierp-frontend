4685faef31534d4e7c801071aa34f32e152a022a
VIKASHNAGAR786 <vikashnagar872@gmail.com>
2025-10-12 16:46:29 +0530

    Add BalanceDueComponent and integrate it into AccountsComponent

---
+++ src/app/components/accounts/balance-due/balance-due.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-balance-due',
+  imports: [],
+  templateUrl: './balance-due.component.html',
+  styleUrl: './balance-due.component.css'
+})
+export class BalanceDueComponent {
+
+}
